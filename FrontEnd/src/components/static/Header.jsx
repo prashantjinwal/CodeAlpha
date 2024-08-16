81973a7c823d7143eb795c6823d809a5b0644913
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-08-16 17:02:43 +0530

    Add header.

--- FrontEnd/src/components/static/Header.jsx
+++ FrontEnd/src/components/static/Header.jsx
@@ -1,12 +1,14 @@
 import Logo from "/assets/Logo/Logo.png"
-// import Rectangle from "/assets/header/Rectangle.png"
+import person from "/header/Ladka.png"
+import line from "/header/Vector.png"
+
 function Header(){
     return(
-        <header className="bg-[#F8F8FD]">
+        <header className="bg-[#eaeaf7]">
             <nav className="flex items-center justify-between py-2 px-[5em] ">
                <div className="flex items-center py-2 gap-[3em]">
 
-                <h3 className="text-[#25324B] text-2xl font-bold flex gap-2 " > <img src={Logo} alt="Logo"/> JobHuntly</h3>
+                <h3 className="text-[#25324B] text-2xl font-bold flex gap-2 " > <img src={Logo} alt="Logo"/> JobHuntly </h3>
             
                     <ul className="flex gap-5">
                         <li className='text-[#515B6F]'>Find a Job</li>
@@ -20,15 +22,21 @@ function Header(){
                 </div>
             </nav>
 
+            <section className="flex gap-4">
+
             <div className="p-[5em] flex flex-col justify-start  ml-4 ">
-                <h3 className="text-5xl font-bold text-[#25324B] font-head">Discover<br/> more than <br/> <span className="text-[#26A4FF]">5000+ Jobs</span></h3>
-                <p className='text-[#515B6F] text-wrap w-1/3 py-2'>Great platform for the job seeker that searching for new career heights and passionate about startups.</p>
-            </div>
 
-            <div className="backgroundRectangle">
+                <h3 className="text-7xl font-bold text-[#25324B] font-head">Discover<br/> more than <br/> <span className="text-[#26A4FF]">5000+ Jobs</span> <br/> <img src={line} alt="vector"/></h3>
+                <p className='text-[#515B6F] text-wrap w-2/3 py-2'>Great platform for the job seeker that searching for new career heights and passionate about startups.</p>
 
             </div>
 
+            <div className="">
+                <img className="w-[85%]" src={person} alt="Person" />
+            </div>
+
+            </section>
+            
 
 
 
